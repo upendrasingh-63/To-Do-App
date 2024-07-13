eb9034a8c959429ac25b9d786cbb1f4c3abe5dd6
Upendra Singh <126307145+upendrasingh-63@users.noreply.github.com>
2024-07-13 16:46:50 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'upendrasingh1234', // Replace with your MySQL root password
-  database: 'todolist' // Replace with your database name
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
-db.connect((err) => {
+connection.connect((err) => {
   if (err) {
     console.error('Error connecting to MySQL:', err);
     return;
   }
-  console.log('MySQL Connected...');
+  console.log('Connected to MySQL');
 });
 
 module.exports = db;
